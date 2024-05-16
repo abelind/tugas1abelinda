15295dea338b7d3a744f29acd91bf9c498513ac8
abelind <abelgude23@gmail.com>
2024-05-16 14:18:26 +0800

    Add files via upload
    belajar

---
+++ home.dart
@@ -0,0 +1,13 @@
+void main(){
+  int lingkarperut = 27;
+
+if (lingkarperut >= 30 ){
+  print("hello word");
+  } else { 
+    print("lingkar perut"); 
+  }
+  /*
+  *ini hanyalah sebuah test
+  *
+  */
+  } 
